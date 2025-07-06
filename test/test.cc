5cd4ba570b9f5cb35eb6e67706f68d03f8b74e47
Mirco Mannino <mircomannino1997@gmail.com>
2025-07-06 19:02:22 +0100

    update test

--- test/test.cc
+++ test/test.cc
@@ -1,23 +1,39 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <errno.h>
 
-int main()
-{
-    size_t N = 1024 * 1024;
-    char A[N];
-    char B[N];
-    char C[N];
+int main() {
+    size_t length = 256 * 1024 * 1024; // 256 MB
+    // mmap anonymous private
+    void *addr = mmap(NULL, length, PROT_READ | PROT_WRITE,
+                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    printf("Allocated %zu bytes at %p\n", length, addr);
+
+    // Touch the memory to actually allocate pages
+    memset(addr, 0, length);
+
+    // Advise kernel to use transparent huge pages
+    if (madvise(addr, length, MADV_HUGEPAGE) != 0) {
+        perror("madvise");
+        munmap(addr, length);
+        return 1;
+    }
+
+    printf("Called madvise with MADV_HUGEPAGE\n");
 
     while (true)
     {
-        for (size_t i = 0; i < N; i++) {
-            A[i] = 1;
-            B[i] = 2;
-        }
-
-        for (size_t i = 0; i < N; i++) {
-            C[i] = A[i] + B[i];
-        }
     }
     
+
+    munmap(addr, length);
     return 0;
 }
